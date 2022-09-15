ac812d1790ae9c2f8ec1110f74eeb43311526760
hoho <kumass2021@gmail.com>
2022-09-15 17:08:29 +0900

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn.functional as F
+import pandas
+
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+class CustomDataset(Dataset):
+    def __init__(self):
+        self.x_data =
+    def __len__(self):
+
+    def __getitem__(self, idx):
